07f6a884898c7a63b7d12b521e2f6c964043b7d6
Recai Oktaş <roktas@gmail.com>
2024-06-25 16:09:24 +0300

    .

--- external/lib/sevgi/external/exec.rb
+++ external/lib/sevgi/external/exec.rb
@@ -5,10 +5,10 @@ require "sevgi"
 def Exec(file, *args, **kwargs)
   location = Sevgi.locate(file, ::File.dirname(exclude = caller_locations(1..1).first.path), exclude:)
 
-  Sevgi::Sandbox.run(location.file) do |this|
+  Sevgi::Sandbox.run(location.file) do |mod|
     include Sevgi::External
 
-    this.const_set(:ARGA, args).freeze
-    this.const_set(:ARGH, kwargs).freeze
+    mod.const_set(:ARGA, args).freeze
+    mod.const_set(:ARGH, kwargs).freeze
   end
 end
