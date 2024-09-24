2d0aa7326962a62e4bf697afda8e8b9b62e43536
Recai Oktaş <roktas@gmail.com>
2024-09-24 09:45:44 +0300

    .

--- graphics/lib/sevgi/graphics/mixtures/tile.rb
+++ graphics/lib/sevgi/graphics/mixtures/tile.rb
@@ -4,89 +4,94 @@ module Sevgi
   module Graphics
     module Mixtures
       module Tile
-        DEFAULT_PROC = proc do |element, href, *positions|
+        PROC = proc do |element, href, *positions|
           element[:id] = [ href, *positions.compact ].join("-")
         end
 
         # rubocop:disable Layout/MultilineArrayLineBreaks,Metrics/MethodLength
 
-        def Tile(id = nil, nx: Undefined, dx: Undefined, ox: 0, ny: Undefined, dy: Undefined, oy: 0, &block)
-          Assert.(tiled = id || self[:id], nx:, dx:, ox:, ny:, dy:, oy:)
+        def Tile(id = Undefined, nx: Undefined, dx: Undefined, ox: 0, ny: Undefined, dy: Undefined, oy: 0, &block)
+          Assert.(id:, nx:, dx:, ox:, ny:, dy:, oy:)
 
-          href, proc, coords = tiled, block || DEFAULT_PROC, proc do |x, y|
+          href, coords = id, proc do |x, y|
             x.zero? ? (y.zero? ? {} : { y: }) : (y.zero? ? { x: } : { x:, y: }) # for pretty kwargs handling
           end
 
-          public_send(id.nil? ? :With : :Within) do
+          defs { g(id:, &block) } if block
+
+          Within do
             ny.times do |i|
               nx.times do |j|
-                proc.(use(href: "##{href}", **coords.(j * dx + ox, i * dy + oy)), href, i + 1, j + 1)
+                PROC.(use(href: "##{href}", **coords.(j * dx + ox, i * dy + oy)), href, i + 1, j + 1)
               end
             end
           end
         end
 
-        def TileX(id = nil, n: Undefined, d: Undefined, o: 0, &block)
-          Assert.(tiled = id || self[:id], n:, d:, o:)
+        def TileX(id = Undefined, n: Undefined, d: Undefined, o: 0, &block)
+          Assert.(id:, n:, d:, o:)
 
-          href, proc, coords = tiled, block || DEFAULT_PROC, proc do |x|
+          href, coords = id, proc do |x|
             x.zero? ? {} : { x: } # for pretty kwargs handling
           end
 
-          public_send(id.nil? ? :With : :Within) do
+          defs { g(id:, &block) } if block
+
+          Within do
             n.times do |i|
-              proc.(use(href: "##{href}", **coords.(i * d + o)), href, i + 1)
+              PROC.(use(href: "##{href}", **coords.(i * d + o)), href, i + 1)
             end
           end
         end
 
-        def TileY(id = nil, n: Undefined, d: Undefined, o: 0, &block)
-          Assert.(tiled = id ||self[:id], n:, d:, o:)
+        def TileY(id = Undefined, n: Undefined, d: Undefined, o: 0, &block)
+          Assert.(id:, n:, d:, o:)
 
-          href, proc, coords = tiled, block || DEFAULT_PROC, proc do |y|
+          href, coords = id, proc do |y|
             y.zero? ? {} : { y: } # for pretty kwargs handling
           end
 
-          public_send(id.nil? ? :With : :Within) do
+          defs { g(id:, &block) } if block
+
+          Within do
             n.times do |i|
-              proc.(use(href: "##{href}", **coords.(i * d + o)), href, i + 1)
+              PROC.(use(href: "##{href}", **coords.(i * d + o)), href, i + 1)
             end
           end
         end
-      end
 
-      module Assert
-        extend self
+        module Assert
+          extend self
 
-        ASSERTION = {
-          n:  proc { |name, value| "Argument '#{name}' must be a positive integer" unless value.positive?        },
-          nx: proc { |name, value| "Argument '#{name}' must be a positive integer" unless value.positive?        },
-          ny: proc { |name, value| "Argument '#{name}' must be a positive integer" unless value.positive?        },
-          d:  proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric) },
-          dx: proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric) },
-          dy: proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric) },
-          o:  proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric) },
-          ox: proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric) },
-          oy: proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric) }
-        }.freeze
+          ASSERTION = {
+            id: proc { |name, value| "Argument '#{name}' must be a string"           unless value.is_a?(::String)  },
+            n:  proc { |name, value| "Argument '#{name}' must be a positive integer" unless value.positive?        },
+            nx: proc { |name, value| "Argument '#{name}' must be a positive integer" unless value.positive?        },
+            ny: proc { |name, value| "Argument '#{name}' must be a positive integer" unless value.positive?        },
+            d:  proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric) },
+            dx: proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric) },
+            dy: proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric) },
+            o:  proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric) },
+            ox: proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric) },
+            oy: proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric) }
+          }.freeze
 
-        def call(id, **kwargs)
-          ArgumentError.("Tiled element must have an id") unless id
+          def call(**kwargs)
+            kwargs.each do |name, value|
+              issue = "Argument '#{name}' required" if value == Undefined
 
-          kwargs.each do |name, value|
-            issue = "Argument '#{name}' required" if value == Undefined
+              unless issue
+                next unless (assertion = ASSERTION[name])
+                next unless (issue = assertion.call(name, value))
+              end
 
-            unless issue
-              next unless (assertion = ASSERTION[name])
-              next unless (issue = assertion.call(name, value))
+              ArgumentError.(issue)
             end
-
-            ArgumentError.(issue)
           end
         end
-      end
 
-      private_constant :Assert
+        private_constant :Assert
+      end
     end
   end
 end
--- graphics/test/graphics/mixtures/tile_test.rb
+++ graphics/test/graphics/mixtures/tile_test.rb
@@ -11,17 +11,19 @@ module Sevgi
         def test_tile_no_id_raises_exception
           error = assert_raises(ArgumentError) do
             SVG DOC do
-              rect(width: 5, height: 8).Tile(nx: 1, dx: 1, ny: 1, dy: 4)
+              rect(id: "rect", width: 5, height: 8)
+              Tile(nx: 1, dx: 1, ny: 1, dy: 4)
             end
           end
 
-          assert_match(/\bmust\b.*\bid\b/, error.message)
+          assert_match(/\bid\b.*\brequired\b/, error.message)
         end
 
         def test_tile_zero_nx_raises_exception
           error = assert_raises(ArgumentError) do
             SVG DOC do
-              rect(id: "rect", width: 5, height: 8).Tile(nx: 0, dx: 1, ny: 1, dy: 4)
+              rect(id: "rect", width: 5, height: 8)
+              Tile("rect", nx: 0, dx: 1, ny: 1, dy: 4)
             end
           end
 
@@ -31,7 +33,8 @@ module Sevgi
         def test_tile_zero_ny_raises_exception
           error = assert_raises(ArgumentError) do
             SVG DOC do
-              rect(id: "rect", width: 5, height: 8).Tile(nx: 1, dx: 1, ny: 0, dy: 4)
+              rect(id: "rect", width: 5, height: 8)
+              Tile("rect", nx: 1, dx: 1, ny: 0, dy: 4)
             end
           end
 
@@ -47,7 +50,8 @@ module Sevgi
           SVG
 
           actual = SVG DOC do
-            rect(id: "rect", width: 5, height: 8).Tile(nx: 1, dx: 1, ny: 1, dy: 4)
+            rect(id: "rect", width: 5, height: 8)
+            Tile("rect", nx: 1, dx: 1, ny: 1, dy: 4)
           end.Render
 
           assert_equal(expected, actual)
@@ -62,13 +66,14 @@ module Sevgi
           SVG
 
           actual = SVG DOC do
-            rect(id: "rect", width: 5, height: 8).Tile(nx: 1, dx: 1, ox: 3, ny: 1, dy: 4, oy: 5)
+            rect(id: "rect", width: 5, height: 8)
+            Tile("rect", nx: 1, dx: 1, ox: 3, ny: 1, dy: 4, oy: 5)
           end.Render
 
           assert_equal(expected, actual)
         end
 
-        def test_tile_multiple
+        def test_tile_without_block
           expected = <<~SVG.chomp
             <svg>
               <rect id="rect" width="5" height="8"/>
@@ -82,32 +87,32 @@ module Sevgi
           SVG
 
           actual = SVG DOC do
-            rect(id: "rect", width: 5, height: 8).Tile(nx: 2, dx: 1, ny: 3, dy: 4)
+            rect(id: "rect", width: 5, height: 8)
+            Tile("rect", nx: 2, dx: 1, ny: 3, dy: 4)
           end.Render
 
           assert_equal(expected, actual)
         end
 
-        def test_tile_by_id
+        def test_tile_with_block
           expected = <<~SVG.chomp
             <svg>
-              <rect id="rect" width="5" height="8"/>
-              <g>
-                <use id="rect-1-1" href="#rect"/>
-                <use id="rect-1-2" href="#rect" x="1"/>
-                <use id="rect-2-1" href="#rect" y="4"/>
-                <use id="rect-2-2" href="#rect" x="1" y="4"/>
-                <use id="rect-3-1" href="#rect" y="8"/>
-                <use id="rect-3-2" href="#rect" x="1" y="8"/>
-              </g>
+              <defs>
+                <g id="rect">
+                  <rect width="5" height="8"/>
+                </g>
+              </defs>
+              <use id="rect-1-1" href="#rect"/>
+              <use id="rect-1-2" href="#rect" x="1"/>
+              <use id="rect-2-1" href="#rect" y="4"/>
+              <use id="rect-2-2" href="#rect" x="1" y="4"/>
+              <use id="rect-3-1" href="#rect" y="8"/>
+              <use id="rect-3-2" href="#rect" x="1" y="8"/>
             </svg>
           SVG
 
           actual = SVG DOC do
-            rect(id: "rect", width: 5, height: 8)
-            g do
-              Tile("rect", nx: 2, dx: 1, ny: 3, dy: 4)
-            end
+            Tile("rect", nx: 2, dx: 1, ny: 3, dy: 4) { rect(width: 5, height: 8) }
           end.Render
 
           assert_equal(expected, actual)
@@ -120,17 +125,19 @@ module Sevgi
         def test_tile_x_no_id_raises_exception
           error = assert_raises(ArgumentError) do
             SVG DOC do
-              rect(width: 5, height: 8).TileX(n: 1, d: 1)
+              rect(id: "rect", width: 5, height: 8)
+              TileX(n: 1, d: 1)
             end
           end
 
-          assert_match(/\bmust\b.*\bid\b/, error.message)
+          assert_match(/\bid\b.*\brequired\b/, error.message)
         end
 
         def test_tile_x_zero_n_raises_exception
           error = assert_raises(ArgumentError) do
             SVG DOC do
-              rect(id: "rect", width: 5, height: 8).TileX(n: 0, d: 1)
+              rect(id: "rect", width: 5, height: 8)
+              TileX("rect", n: 0, d: 1)
             end
           end
 
@@ -146,7 +153,8 @@ module Sevgi
           SVG
 
           actual = SVG DOC do
-            rect(id: "rect", width: 5, height: 8).TileX(n: 1, d: 1)
+            rect(id: "rect", width: 5, height: 8)
+            TileX("rect", n: 1, d: 1)
           end.Render
 
           assert_equal(expected, actual)
@@ -161,13 +169,14 @@ module Sevgi
           SVG
 
           actual = SVG DOC do
-            rect(id: "rect", width: 5, height: 8).TileX(n: 1, d: 1, o: 3)
+            rect(id: "rect", width: 5, height: 8)
+            TileX("rect", n: 1, d: 1, o: 3)
           end.Render
 
           assert_equal(expected, actual)
         end
 
-        def test_tile_x_multiple
+        def test_tile_x_without_block
           expected = <<~SVG.chomp
             <svg>
               <rect id="rect" width="5" height="8"/>
@@ -177,26 +186,28 @@ module Sevgi
           SVG
 
           actual = SVG DOC do
-            rect(id: "rect", width: 5, height: 8).TileX(n: 2, d: 1)
+            rect(id: "rect", width: 5, height: 8)
+            TileX("rect", n: 2, d: 1)
           end.Render
 
           assert_equal(expected, actual)
         end
 
-        def test_tile_x_by_id
+        def test_tile_x_with_block
           expected = <<~SVG.chomp
             <svg>
-              <rect id="rect" width="5" height="8"/>
-              <g>
-                <use id="rect-1" href="#rect"/>
-                <use id="rect-2" href="#rect" x="1"/>
-              </g>
+              <defs>
+                <g id="rect">
+                  <rect width="5" height="8"/>
+                </g>
+              </defs>
+              <use id="rect-1" href="#rect"/>
+              <use id="rect-2" href="#rect" x="1"/>
             </svg>
           SVG
 
           actual = SVG DOC do
-            rect(id: "rect", width: 5, height: 8)
-            g { TileX("rect", n: 2, d: 1) }
+            TileX("rect", n: 2, d: 1) { rect(width: 5, height: 8) }
           end.Render
 
           assert_equal(expected, actual)
@@ -209,17 +220,19 @@ module Sevgi
         def test_tile_y_no_id_raises_exception
           error = assert_raises(ArgumentError) do
             SVG DOC do
-              rect(width: 5, height: 8).TileY(n: 1, d: 1)
+              rect(id: "rect", width: 5, height: 8)
+              TileY(n: 1, d: 1)
             end
           end
 
-          assert_match(/\bmust\b.*\bid\b/, error.message)
+          assert_match(/\bid\b.*\brequired\b/, error.message)
         end
 
         def test_tile_y_zero_n_raises_exception
           error = assert_raises(ArgumentError) do
             SVG DOC do
-              rect(id: "rect", width: 5, height: 8).TileY(n: 0, d: 1)
+              rect(id: "rect", width: 5, height: 8)
+              TileY("rect", n: 0, d: 1)
             end
           end
 
@@ -235,7 +248,8 @@ module Sevgi
           SVG
 
           actual = SVG DOC do
-            rect(id: "rect", width: 5, height: 8).TileY(n: 1, d: 1)
+            rect(id: "rect", width: 5, height: 8)
+            TileY("rect", n: 1, d: 1)
           end.Render
 
           assert_equal(expected, actual)
@@ -250,7 +264,8 @@ module Sevgi
           SVG
 
           actual = SVG DOC do
-            rect(id: "rect", width: 5, height: 8).TileY(n: 1, d: 1, o: 3)
+            rect(id: "rect", width: 5, height: 8)
+            TileY("rect", n: 1, d: 1, o: 3)
           end.Render
 
           assert_equal(expected, actual)
@@ -266,26 +281,28 @@ module Sevgi
           SVG
 
           actual = SVG DOC do
-            rect(id: "rect", width: 5, height: 8).TileY(n: 2, d: 1)
+            rect(id: "rect", width: 5, height: 8)
+            TileY("rect", n: 2, d: 1)
           end.Render
 
           assert_equal(expected, actual)
         end
 
-        def test_tile_y_by_id
+        def test_tile_y_with_block
           expected = <<~SVG.chomp
             <svg>
-              <rect id="rect" width="5" height="8"/>
-              <g>
-                <use id="rect-1" href="#rect"/>
-                <use id="rect-2" href="#rect" y="1"/>
-              </g>
+              <defs>
+                <g id="rect">
+                  <rect width="5" height="8"/>
+                </g>
+              </defs>
+              <use id="rect-1" href="#rect"/>
+              <use id="rect-2" href="#rect" y="1"/>
             </svg>
           SVG
 
           actual = SVG DOC do
-            rect(id: "rect", width: 5, height: 8)
-            g { TileY("rect", n: 2, d: 1) }
+            TileY("rect", n: 2, d: 1) { rect(width: 5, height: 8) }
           end.Render
 
           assert_equal(expected, actual)
