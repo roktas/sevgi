f2e2545b01a636ade628f15c4f0ba515af7f6951
Recai Oktaş <roktas@gmail.com>
2025-05-16 15:25:28 +0300

    .

--- graphics/lib/sevgi/graphics/mixtures/tile.rb
+++ graphics/lib/sevgi/graphics/mixtures/tile.rb
@@ -3,15 +3,11 @@
 module Sevgi
   module Graphics
     module Mixtures
-      module Tile
-        PROC = proc do |element, href, *positions|
-          element[:id] = [ href, *positions.compact ].join("-")
-        end
+      module Tile # rubocop:disable Layout/MultilineArrayLineBreaks,Metrics/MethodLength
+        PREFIX = "tile"
 
-        # rubocop:disable Layout/MultilineArrayLineBreaks,Metrics/MethodLength
-
-        def Tile(id = Undefined, nx: Undefined, dx: Undefined, ox: 0, ny: Undefined, dy: Undefined, oy: 0, &block)
-          Assert.(id:, nx:, dx:, ox:, ny:, dy:, oy:)
+        def Tile(id = Undefined, nx: Undefined, dx: Undefined, ox: 0, ny: Undefined, dy: Undefined, oy: 0, proc: nil, &block)
+          Helper.assert(id:, nx:, dx:, ox:, ny:, dy:, oy:, proc:)
 
           href, coords = id, proc do |x, y|
             x.zero? ? (y.zero? ? {} : { y: }) : (y.zero? ? { x: } : { x:, y: }) # for pretty kwargs handling
@@ -20,16 +16,28 @@ module Sevgi
           defs { g(id:, &block) } if block
 
           Within do
-            ny.times do |i|
-              nx.times do |j|
-                PROC.(use(href: "##{href}", **coords.(j * dx + ox, i * dy + oy)), href, i + 1, j + 1)
+            ny.times do |y|
+              rs = Helper.classify(as: "row", index: y, upper: ny)
+
+              nx.times do |x|
+                cs = Helper.classify(as: "col", index: x, upper: nx)
+
+                element = use(
+                  id:    [ href, y + 1, x + 1 ].join("-"),
+                  href:  "##{href}",
+                  class: [ *rs, *cs ].join(" "),
+                  **coords.(x * dx + ox, y * dy + oy)
+                )
+                proc.call(element:, x:, y:, nx:, ny:) if proc
               end
             end
           end
         end
 
-        def TileX(id = Undefined, n: Undefined, d: Undefined, o: 0, &block)
-          Assert.(id:, n:, d:, o:)
+        def Tile!(id, klass = PREFIX, ...) =  g(id:, class: klass) { Tile(id, ...) }
+
+        def TileX(id = Undefined, n: Undefined, d: Undefined, o: 0, proc: nil, &block)
+          Helper.assert(id:, n:, d:, o:, proc:)
 
           href, coords = id, proc do |x|
             x.zero? ? {} : { x: } # for pretty kwargs handling
@@ -38,14 +46,24 @@ module Sevgi
           defs { g(id:, &block) } if block
 
           Within do
-            n.times do |i|
-              PROC.(use(href: "##{href}", **coords.(i * d + o)), href, i + 1)
+            n.times do |x|
+              cs = Helper.classify(as: "col", index: x, upper: n)
+
+              element = use(
+                id:    [ href, x + 1 ].join("-"),
+                href:  "##{href}",
+                class: cs.join(" "),
+                **coords.(x * d + o)
+              )
+              proc.call(element:, x:, n:) if proc
             end
           end
         end
 
-        def TileY(id = Undefined, n: Undefined, d: Undefined, o: 0, &block)
-          Assert.(id:, n:, d:, o:)
+        def TileX!(id, klass = PREFIX, ...) =  g(id:, class: klass) { TileX(id, ...) }
+
+        def TileY(id = Undefined, n: Undefined, d: Undefined, o: 0, proc: nil, &block)
+          Helper.assert(id:, n:, d:, o:, proc:)
 
           href, coords = id, proc do |y|
             y.zero? ? {} : { y: } # for pretty kwargs handling
@@ -54,29 +72,40 @@ module Sevgi
           defs { g(id:, &block) } if block
 
           Within do
-            n.times do |i|
-              PROC.(use(href: "##{href}", **coords.(i * d + o)), href, i + 1)
+            n.times do |y|
+              rs = Helper.classify(as: "row", index: y, upper: n)
+
+              element = use(
+                id:    [ href, y + 1 ].join("-"),
+                href:  "##{href}",
+                class: rs.join(" "),
+                **coords.(y * d + o)
+              )
+              proc.call(element:, y:, n:) if proc
             end
           end
         end
 
-        module Assert
+        def TileY!(id, klass = PREFIX, ...) =  g(id:, class: klass) { TileY(id, ...) }
+
+        module Helper
           extend self
 
           ASSERTION = {
-            id: proc { |name, value| "Argument '#{name}' must be a string"           unless value.is_a?(::String)  },
-            n:  proc { |name, value| "Argument '#{name}' must be a positive integer" unless value.positive?        },
-            nx: proc { |name, value| "Argument '#{name}' must be a positive integer" unless value.positive?        },
-            ny: proc { |name, value| "Argument '#{name}' must be a positive integer" unless value.positive?        },
-            d:  proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric) },
-            dx: proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric) },
-            dy: proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric) },
-            o:  proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric) },
-            ox: proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric) },
-            oy: proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric) }
+            id:   proc { |name, value| "Argument '#{name}' must be a string"           unless value.is_a?(::String)             },
+            n:    proc { |name, value| "Argument '#{name}' must be a positive integer" unless value.positive?                   },
+            nx:   proc { |name, value| "Argument '#{name}' must be a positive integer" unless value.positive?                   },
+            ny:   proc { |name, value| "Argument '#{name}' must be a positive integer" unless value.positive?                   },
+            d:    proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric)            },
+            dx:   proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric)            },
+            dy:   proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric)            },
+            o:    proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric)            },
+            ox:   proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric)            },
+            oy:   proc { |name, value| "Argument '#{name}' must be a number"           unless value.is_a?(::Numeric)            },
+            proc: proc { |name, value| "Argument '#{name}' must be a proc"             unless value.nil? || value.is_a?(::Proc) }
           }.freeze
 
-          def call(**kwargs)
+          def assert(**kwargs)
             kwargs.each do |name, value|
               issue = "Argument '#{name}' required" if value == Undefined
 
@@ -88,9 +117,17 @@ module Sevgi
               ArgumentError.(issue)
             end
           end
+
+          def classify(as:, index:, upper:)
+            [].tap do |classes|
+              classes << "#{PREFIX}-#{as}-#{index + 1}"
+              classes << "#{PREFIX}-#{as}-first" if index.zero?
+              classes << "#{PREFIX}-#{as}-last"  if index + 1 == upper
+            end
+          end
         end
 
-        private_constant :Assert
+        private_constant :Helper
       end
     end
   end
--- graphics/test/graphics/mixtures/tile_test.rb
+++ graphics/test/graphics/mixtures/tile_test.rb
@@ -45,7 +45,7 @@ module Sevgi
           expected = <<~SVG.chomp
             <svg>
               <rect id="rect" width="5" height="8"/>
-              <use id="rect-1-1" href="#rect"/>
+              <use id="rect-1-1" href="#rect" class="tile-row-1 tile-row-first tile-row-last tile-col-1 tile-col-first tile-col-last"/>
             </svg>
           SVG
 
@@ -61,7 +61,7 @@ module Sevgi
           expected = <<~SVG.chomp
             <svg>
               <rect id="rect" width="5" height="8"/>
-              <use id="rect-1-1" href="#rect" x="3" y="5"/>
+              <use id="rect-1-1" href="#rect" class="tile-row-1 tile-row-first tile-row-last tile-col-1 tile-col-first tile-col-last" x="3" y="5"/>
             </svg>
           SVG
 
@@ -77,12 +77,12 @@ module Sevgi
           expected = <<~SVG.chomp
             <svg>
               <rect id="rect" width="5" height="8"/>
-              <use id="rect-1-1" href="#rect"/>
-              <use id="rect-1-2" href="#rect" x="1"/>
-              <use id="rect-2-1" href="#rect" y="4"/>
-              <use id="rect-2-2" href="#rect" x="1" y="4"/>
-              <use id="rect-3-1" href="#rect" y="8"/>
-              <use id="rect-3-2" href="#rect" x="1" y="8"/>
+              <use id="rect-1-1" href="#rect" class="tile-row-1 tile-row-first tile-col-1 tile-col-first"/>
+              <use id="rect-1-2" href="#rect" class="tile-row-1 tile-row-first tile-col-2 tile-col-last" x="1"/>
+              <use id="rect-2-1" href="#rect" class="tile-row-2 tile-col-1 tile-col-first" y="4"/>
+              <use id="rect-2-2" href="#rect" class="tile-row-2 tile-col-2 tile-col-last" x="1" y="4"/>
+              <use id="rect-3-1" href="#rect" class="tile-row-3 tile-row-last tile-col-1 tile-col-first" y="8"/>
+              <use id="rect-3-2" href="#rect" class="tile-row-3 tile-row-last tile-col-2 tile-col-last" x="1" y="8"/>
             </svg>
           SVG
 
@@ -102,17 +102,40 @@ module Sevgi
                   <rect width="5" height="8"/>
                 </g>
               </defs>
-              <use id="rect-1-1" href="#rect"/>
-              <use id="rect-1-2" href="#rect" x="1"/>
-              <use id="rect-2-1" href="#rect" y="4"/>
-              <use id="rect-2-2" href="#rect" x="1" y="4"/>
-              <use id="rect-3-1" href="#rect" y="8"/>
-              <use id="rect-3-2" href="#rect" x="1" y="8"/>
+              <use id="rect-1-1" href="#rect" class="tile-row-1 tile-row-first tile-col-1 tile-col-first"/>
+              <use id="rect-1-2" href="#rect" class="tile-row-1 tile-row-first tile-col-2 tile-col-last" x="1"/>
+              <use id="rect-2-1" href="#rect" class="tile-row-2 tile-col-1 tile-col-first" y="4"/>
+              <use id="rect-2-2" href="#rect" class="tile-row-2 tile-col-2 tile-col-last" x="1" y="4"/>
+              <use id="rect-3-1" href="#rect" class="tile-row-3 tile-row-last tile-col-1 tile-col-first" y="8"/>
+              <use id="rect-3-2" href="#rect" class="tile-row-3 tile-row-last tile-col-2 tile-col-last" x="1" y="8"/>
             </svg>
           SVG
 
           actual = SVG DOC do
             Tile("rect", nx: 2, dx: 1, ny: 3, dy: 4) { rect(width: 5, height: 8) }
+          end.Render
+
+          assert_equal(expected, actual)
+        end
+
+        def test_tile_bang
+          expected = <<~SVG.chomp
+            <svg>
+              <rect id="rect" width="5" height="8"/>
+              <g id="myrect" class="mytile">
+                <use id="myrect-1-1" href="#myrect" class="tile-row-1 tile-row-first tile-col-1 tile-col-first"/>
+                <use id="myrect-1-2" href="#myrect" class="tile-row-1 tile-row-first tile-col-2 tile-col-last" x="1"/>
+                <use id="myrect-2-1" href="#myrect" class="tile-row-2 tile-col-1 tile-col-first" y="4"/>
+                <use id="myrect-2-2" href="#myrect" class="tile-row-2 tile-col-2 tile-col-last" x="1" y="4"/>
+                <use id="myrect-3-1" href="#myrect" class="tile-row-3 tile-row-last tile-col-1 tile-col-first" y="8"/>
+                <use id="myrect-3-2" href="#myrect" class="tile-row-3 tile-row-last tile-col-2 tile-col-last" x="1" y="8"/>
+              </g>
+            </svg>
+          SVG
+
+          actual = SVG DOC do
+            rect(id: "rect", width: 5, height: 8)
+            Tile!("myrect", "mytile", nx: 2, dx: 1, ny: 3, dy: 4)
           end.Render
 
           assert_equal(expected, actual)
@@ -148,7 +171,7 @@ module Sevgi
           expected = <<~SVG.chomp
             <svg>
               <rect id="rect" width="5" height="8"/>
-              <use id="rect-1" href="#rect"/>
+              <use id="rect-1" href="#rect" class="tile-col-1 tile-col-first tile-col-last"/>
             </svg>
           SVG
 
@@ -164,7 +187,7 @@ module Sevgi
           expected = <<~SVG.chomp
             <svg>
               <rect id="rect" width="5" height="8"/>
-              <use id="rect-1" href="#rect" x="3"/>
+              <use id="rect-1" href="#rect" class="tile-col-1 tile-col-first tile-col-last" x="3"/>
             </svg>
           SVG
 
@@ -180,8 +203,8 @@ module Sevgi
           expected = <<~SVG.chomp
             <svg>
               <rect id="rect" width="5" height="8"/>
-              <use id="rect-1" href="#rect"/>
-              <use id="rect-2" href="#rect" x="1"/>
+              <use id="rect-1" href="#rect" class="tile-col-1 tile-col-first"/>
+              <use id="rect-2" href="#rect" class="tile-col-2 tile-col-last" x="1"/>
             </svg>
           SVG
 
@@ -201,8 +224,8 @@ module Sevgi
                   <rect width="5" height="8"/>
                 </g>
               </defs>
-              <use id="rect-1" href="#rect"/>
-              <use id="rect-2" href="#rect" x="1"/>
+              <use id="rect-1" href="#rect" class="tile-col-1 tile-col-first"/>
+              <use id="rect-2" href="#rect" class="tile-col-2 tile-col-last" x="1"/>
             </svg>
           SVG
 
@@ -243,7 +266,7 @@ module Sevgi
           expected = <<~SVG.chomp
             <svg>
               <rect id="rect" width="5" height="8"/>
-              <use id="rect-1" href="#rect"/>
+              <use id="rect-1" href="#rect" class="tile-row-1 tile-row-first tile-row-last"/>
             </svg>
           SVG
 
@@ -259,7 +282,7 @@ module Sevgi
           expected = <<~SVG.chomp
             <svg>
               <rect id="rect" width="5" height="8"/>
-              <use id="rect-1" href="#rect" y="3"/>
+              <use id="rect-1" href="#rect" class="tile-row-1 tile-row-first tile-row-last" y="3"/>
             </svg>
           SVG
 
@@ -275,8 +298,8 @@ module Sevgi
           expected = <<~SVG.chomp
             <svg>
               <rect id="rect" width="5" height="8"/>
-              <use id="rect-1" href="#rect"/>
-              <use id="rect-2" href="#rect" y="1"/>
+              <use id="rect-1" href="#rect" class="tile-row-1 tile-row-first"/>
+              <use id="rect-2" href="#rect" class="tile-row-2 tile-row-last" y="1"/>
             </svg>
           SVG
 
@@ -296,8 +319,8 @@ module Sevgi
                   <rect width="5" height="8"/>
                 </g>
               </defs>
-              <use id="rect-1" href="#rect"/>
-              <use id="rect-2" href="#rect" y="1"/>
+              <use id="rect-1" href="#rect" class="tile-row-1 tile-row-first"/>
+              <use id="rect-2" href="#rect" class="tile-row-2 tile-row-last" y="1"/>
             </svg>
           SVG
 
