d7cd0f9300fbbb5c8aad60c6c9a2890045d84c21
Recai Oktaş <roktas@gmail.com>
2025-12-11 18:58:53 +0300

    .

--- external/lib/sevgi/internal.rb
+++ external/lib/sevgi/internal.rb
@@ -1,7 +1,4 @@
 # frozen_string_literal: true
 
-require "sevgi/function"
-
-require_relative "internal/file"
 require_relative "internal/locate"
 require_relative "internal/sandbox"
--- function/lib/sevgi/function/file.rb
+++ function/lib/sevgi/function/file.rb
@@ -16,6 +16,35 @@ module Sevgi
         Digest::SHA1.digest(old_content) != Digest::SHA1.digest(content)
       end
 
+      def existing(file, extensions)
+        return file if ::File.exist?(file)
+        return nil unless ::File.extname(file).empty?
+        return nil if extensions.empty?
+
+        extensions.map { |ext| "#{file}.#{ext}" }.detect { |file| ::File.exist?(file) }
+      end
+
+      def existing!(file, extensions)
+        existing(file, extensions).tap do |found|
+          raise(ArgumentError, "No matching file(s) found: #{file}") unless found
+        end
+      end
+
+      def existings(*files, extensions: [])
+        {}.tap do |existings|
+          files.compact.each { |file| existings[file] = existing(file, extensions) }
+        end
+      end
+
+      def existings!(...)
+        existings = F.existings(...)
+        missings = existings.select { |_, match| match.nil? }.keys
+
+        raise(ArgumentError, "No matching file(s) found: #{missings.join(", ")}") unless missings.empty?
+
+        existings
+      end
+
       def out(content, *paths, &filter)
         if paths.empty?
           ::Kernel.puts(content)
@@ -27,6 +56,12 @@ module Sevgi
 
           file.tap { ::File.write(file, output) }
         end
+      end
+
+      def qualify(file, default_extension)
+        return file unless ::File.extname(file).empty?
+
+        "#{file}.#{default_extension}"
       end
 
       def subext(ext, *paths)
