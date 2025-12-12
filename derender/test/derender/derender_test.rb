eec2391b3a2367f4e09be3eb9217e20b4b95f17f
Recai Oktaş <roktas@gmail.com>
2025-12-12 15:29:33 +0300

    .

--- derender/test/derender/derender_test.rb
+++ derender/test/derender/derender_test.rb
@@ -27,17 +27,37 @@ module Sevgi
         assert_equal(expected, actual)
       end
 
-      def test_derender_render
-        doc = SVG :minimal
-
-        expected = <<~SVG.chomp
+      def test_derender_render_include_current_true
+        expected = xml = <<~SVG.chomp
           <g id="xxx">
             <line id="line1" length="10.0"/>
             <line id="line2" length="20.0"/>
           </g>
         SVG
 
-        actual = Derender.derender(expected, "xxx").(doc).Render
+        actual = Derender.derender(xml, "xxx").(SVG(:minimal)).Render
+
+        assert_equal(expected, actual)
+      end
+
+      def test_derender_render_include_current_false
+        xml = <<~SVG.chomp
+          <g id="xxx">
+            <line id="line1" length="10.0"/>
+            <line id="line2" length="20.0"/>
+          </g>
+        SVG
+
+        expected = <<~SVG.chomp
+          <svg>
+            <line id="line1" length="10.0"/>
+            <line id="line2" length="20.0"/>
+          </svg>
+        SVG
+
+        actual = SVG :minimal do
+          Derender.derender(xml, "xxx").(self, false)
+        end.Render
 
         assert_equal(expected, actual)
       end
