ceb35a70e1180b6ece632b56e5f83c338a43ea98
Recai Oktaş <roktas@gmail.com>
2025-12-16 23:11:44 +0300

    .

--- derender/lib/sevgi/derender.rb
+++ derender/lib/sevgi/derender.rb
@@ -10,17 +10,12 @@ require_relative "derender/node"
 
 module Sevgi
   module Derender
-    def derender_file(file, id: nil)           = Document.load_file(file).(id).derender
-
     def derender(content, id: nil)             = Document.new(content).(id).derender
-
-    def evaluate(content, element, id: nil)    = Document.new(content).(id).evaluate(element)
-
+    def derender_file(file, id: nil)           = Document.load_file(file).(id).derender
     def evaluate!(content, element, id: nil)   = Document.new(content).(id).evaluate!(element)
-
-    def evaluate_file(file, element, id: nil)  = Document.load_file(file).(id).evaluate(element)
-
+    def evaluate(content, element, id: nil)    = Document.new(content).(id).evaluate(element)
     def evaluate_file!(file, element, id: nil) = Document.load_file(file).(id).evaluate!(element)
+    def evaluate_file(file, element, id: nil)  = Document.load_file(file).(id).evaluate(element)
 
     extend self
   end
