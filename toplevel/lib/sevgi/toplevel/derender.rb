0e06a3de4a44c039cd7c76ca3ba209b88034e51c
Recai Oktaş <roktas@gmail.com>
2026-07-06 10:05:05 +0300

    fix(toplevel): remove bang derender wrappers
    Drop unused Decompile! and Derender! wrappers instead of binding them to undefined derender APIs, while preserving the non-bang file delegates.

--- toplevel/lib/sevgi/toplevel/derender.rb
+++ toplevel/lib/sevgi/toplevel/derender.rb
@@ -6,10 +6,6 @@ module Sevgi
   module Toplevel
     def Decompile(file, id) = Derender.decompile_file(file, id:)
 
-    def Decompile!(file, id) = Derender.decompile_file!(file, id:)
-
     def Derender(file, id) = Derender.derender_file(file, id:)
-
-    def Derender!(file, id) = Derender.derender_file!(file, id:)
   end
 end
--- toplevel/test/toplevel/derender_test.rb
+++ toplevel/test/toplevel/derender_test.rb
@@ -4,5 +4,54 @@ require_relative "../test_helper"
 
 module Sevgi
   class ToplevelDerenderTest < Minitest::Test
+    def test_bang_wrappers_are_not_exported
+      object = receiver
+
+      refute_respond_to(object, :Decompile!)
+      refute_respond_to(object, :Derender!)
+    end
+
+    def test_decompile_delegates_to_file_api
+      calls = []
+      node = Object.new
+
+      ::Sevgi::Derender.stub(
+        :decompile_file,
+        -> (file, id:) {
+          calls << [file, id]
+          node
+        }
+      ) do
+        assert_same(node, receiver.Decompile("drawing", "Root"))
+      end
+
+      assert_equal([%w[drawing Root]], calls)
+    end
+
+    def test_derender_delegates_to_file_api
+      calls = []
+
+      ::Sevgi::Derender.stub(
+        :derender_file,
+        -> (file, id:) {
+          calls << [file, id]
+          "SVG"
+        }
+      ) do
+        assert_equal("SVG", receiver.Derender("drawing", "Root"))
+      end
+
+      assert_equal([%w[drawing Root]], calls)
+    end
+
+    private
+
+    def receiver
+      Class
+        .new do
+          include(::Sevgi)
+        end
+        .new
+    end
   end
 end
