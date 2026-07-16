c039bda7490addfa7b7f17508767db8f797f6e0a
Recai Oktaş <roktas@gmail.com>
2026-07-16 11:02:37 +0300

    fix(showcase): fill copperplate guidesheet

--- showcase/test/integration_test.rb
+++ showcase/test/integration_test.rb
@@ -9,6 +9,30 @@ module Sevgi
   EXAMPLES = Test::Suite.new(File.expand_path("#{__dir__}/../srv"))
 
   class IntegrationTest < Minitest::Test
+    def test_guidesheets_share_visible_bounds
+      squared, copperplate = %w[squared copperplate].map do |name|
+        Nokogiri::XML(File.read(File.expand_path("../srv/#{name}.svg", __dir__)))
+      end
+
+      %w[width height viewBox].each do |attribute|
+        assert_equal(squared.root[attribute], copperplate.root[attribute], attribute)
+      end
+
+      frames = [squared, copperplate].map { it.at_xpath("//*[@class=\"frame rule\"]") }
+      %w[x y width height].each do |attribute|
+        assert_equal(frames[0][attribute], frames[1][attribute], attribute)
+      end
+
+      paths = copperplate.xpath("//*[@class=\"rule major horizontal\" or @class=\"rule major vertical\"]")
+      majors = paths.map { |path| path["d"] }
+      [
+        "M 0 0 L 0 30",
+        "M 0 0 L 48 0",
+        "M 0 30 L 48 30",
+        "M 48 0 L 48 30"
+      ].each { assert_includes(majors, it) }
+    end
+
     def test_all_valid_outputs_are_identical
       EXAMPLES.valids.each do |script|
         result = script.run_passive
