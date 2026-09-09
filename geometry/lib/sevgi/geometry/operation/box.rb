c71bf1df3999d29d49157ce752fc6482c9411b2b
Recai Oktaş <roktas@bil.omu.edu.tr>
2026-09-09 12:28:05 +0300

    refactor(geometry): simplify aggregate box operation

--- geometry/lib/sevgi/geometry/operation/box.rb
+++ geometry/lib/sevgi/geometry/operation/box.rb
@@ -16,19 +16,33 @@ module Sevgi
       #   b = Sevgi::Geometry::Line.([-4, 8], [20, 12])
       #   Sevgi::Geometry::Operation.box(a, b) # => Rect spanning both elements
       def box(*elements)
+        validate_box_elements(elements)
+        boxes = elements.map(&:box)
+
+        Rect.from_corners(minimum_corner(boxes), maximum_corner(boxes))
+      end
+
+      private
+
+      def maximum_corner(boxes)
+        [
+          boxes.map { it.position.x + it.width }.max,
+          boxes.map { it.position.y + it.height }.max
+        ]
+      end
+
+      def minimum_corner(boxes)
+        [
+          boxes.map { it.position.x }.min,
+          boxes.map { it.position.y }.min
+        ]
+      end
+
+      def validate_box_elements(elements)
         ArgumentError.("At least one geometric element required") if elements.empty?
 
-        if (invalid = elements.find { !it.is_a?(Element) })
-          OperationInapplicableError.("Not a Geometric Element: #{invalid}")
-        end
-
-        boxes = elements.map(&:box)
-        left = boxes.map { it.position.x }.min
-        top = boxes.map { it.position.y }.min
-        right = boxes.map { it.position.x + it.width }.max
-        bottom = boxes.map { it.position.y + it.height }.max
-
-        Rect.from_corners([left, top], [right, bottom])
+        invalid = elements.find { !it.is_a?(Element) }
+        OperationInapplicableError.("Not a Geometric Element: #{invalid}") if invalid
       end
     end
   end
